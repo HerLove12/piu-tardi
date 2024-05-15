25a681ec226eaea965f0d481454acb4bc7bd02fe
Spagni <spagnimeacci.lorenzo@itismeucci.com>
2024-05-15 17:24:08 +0200

    Risolto err. visual. tutti annunci in utente

--- pages/articolo.php
+++ pages/articolo.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 include("./connessione.php");
 ?>
@@ -7,36 +7,35 @@ include("./connessione.php");
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Sito</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sito</title>
 </head>
 
 <body>
     <?php
-        $id = $_GET["id"];
-        $sql = "SELECT a.nome AS nome, a.descrizioni AS descr, a.foto AS foto, a.datacaricamento AS data, t.nome AS tipo, u.email AS email, u.ID as ID FROM annuncio AS a
+    $id = $_GET["id"];
+    $sql = "SELECT a.nome AS nome, a.descrizioni AS descr, a.foto AS foto, a.datacaricamento AS data, t.nome AS tipo, u.email AS email, u.ID as ID FROM annuncio AS a
         JOIN tipologia AS t ON t.ID = a.ID_tipologia
         JOIN utente AS u ON u.ID = a.ID_utente
         WHERE a.ID = $id";
-        $result = $conn->query($sql);
-        while($row = $result->fetch_assoc()){
-            $n = $row["nome"];
-            $d = $row["descr"];
-            $f = $row["foto"];
-            $data = $row["data"];
-            $e = $row["email"];
-            $t = $row["tipo"];
-            $ID = $row["ID"];
-            $imgURL = "../images/" . $f; // DA DECIDERE LA MODIFICA DELL'IMMAGINE
-            echo "<h1>$n</h1>";
-            echo "<img src=\"$imgURL\" onerror=\"this.src='../images/default.png'\" width=\"200px\" height=\"200px\"><br>";
-            echo "<h3>$d</h3>";
-            echo "<p>Caricata da:<br><a href=\"./utente.php?id=$ID\">$e<a></p>"; //LINK DIRETTO ALL'UTENTE
-            echo "<p>$data / $t</p>";
-        }
+    $result = $conn->query($sql);
+    while ($row = $result->fetch_assoc()) {
+        $n = $row["nome"];
+        $d = $row["descr"];
+        $f = $row["foto"];
+        $data = $row["data"];
+        $e = $row["email"];
+        $t = $row["tipo"];
+        $ID = $row["ID"];
+        $imgURL = "../images/" . $f; // DA DECIDERE LA MODIFICA DELL'IMMAGINE
+        echo "<h1>$n</h1>";
+        echo "<img src=\"$imgURL\" onerror=\"this.src='../images/default.png'\" width=\"200px\" height=\"200px\"><br>";
+        echo "<h3>$d</h3>";
+        echo "<p>Caricata da:<br><a href=\"./utente.php?id=$ID\">$e<a></p>"; //LINK DIRETTO ALL'UTENTE
+        echo "<p>$data / $t</p>";
+    }
     ?>
 </body>
 
 </html>
-
--- pages/index.php
+++ pages/index.php
@@ -7,67 +7,67 @@ include("./connessione.php");
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>negozio</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>negozio</title>
 </head>
 
 <body>
-    <div> 
+    <div>
         <!-- menu a tendina categorie -->
         <?php
-                $sql = "SELECT nome, ID FROM tipologia";
-                $result = $conn->query($sql);
-                echo "<form action=\"index.php\" method=\"GET\">
+        $sql = "SELECT nome, ID FROM tipologia";
+        $result = $conn->query($sql);
+        echo "<form action=\"index.php\" method=\"GET\">
                 <select name=\"filtro\">";
-                echo "<option value=\"0\">-nessun filtro-</option>";
+        echo "<option value=\"0\">-nessun filtro-</option>";
 
-                if($result->num_rows>0){
-                    while($row=$result->fetch_assoc()){
-                        $nome=$row['nome'];
-                        $ID=$row['ID'];
-                        echo "<option value=\"$ID\">$nome</option>";
-                    }
-                    echo "</select>";
-                }
-                echo "<input type=\"submit\">";
-                echo "</form>";
-            ?>
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $nome = $row['nome'];
+                $ID = $row['ID'];
+                echo "<option value=\"$ID\">$nome</option>";
+            }
+            echo "</select>";
+        }
+        echo "<input type=\"submit\">";
+        echo "</form>";
+        ?>
         <div><!-- dashboard articoli -->
             <?php
-                $sql = "SELECT annuncio.ID, annuncio.nome, annuncio.foto, tipologia.nome AS tip, utente.ID AS utID, utente.email AS email FROM annuncio
+            $sql = "SELECT annuncio.ID, annuncio.nome, annuncio.foto, tipologia.nome AS tip, utente.ID AS utID, utente.email AS email FROM annuncio
                         JOIN tipologia ON tipologia.ID = annuncio.ID_tipologia
                         JOIN utente ON utente.ID = annuncio.ID_utente";
 
-                $filtro;
-                if(isset($_GET["filtro"]) and $_GET["filtro"]!=0){
-                    $filtro=$_GET["filtro"];
-                    $sql=$sql." WHERE tipologia.ID=$filtro";
-                }
-                
-                
-                $result = $conn->query($sql);
-                if($result){
-                    if($result->num_rows>0){
-                        while($row = $result->fetch_assoc()){
-                            $nome = $row['nome'];
-                            $foto = $row['foto'];
-                            $tipologia = $row['tip'];
-                            $ID = $row['ID'];
-                            $utID = $row['utID'];
-                            $e = $row['email'];
-                            echo "<div>
+            $filtro;
+            if (isset($_GET["filtro"]) and $_GET["filtro"] != 0) {
+                $filtro = $_GET["filtro"];
+                $sql = $sql . " WHERE tipologia.ID=$filtro";
+            }
+
+
+            $result = $conn->query($sql);
+            if ($result) {
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $nome = $row['nome'];
+                        $foto = $row['foto'];
+                        $tipologia = $row['tip'];
+                        $ID = $row['ID'];
+                        $utID = $row['utID'];
+                        $e = $row['email'];
+                        echo "<div>
                                 <a href=\"./articolo.php?id=$ID\"><img src=\"$foto\" onerror=\"this.src='../images/default.png'\" width=\"200px\" height=\"200px\" \"></a>
                                 <h3>$nome</h3>
                                 <p>$tipologia</p>
                                 <p>Caricata da:<br><a href=\"./utente.php?id=$utID\">$e<a></p> 
-                            </div>";//LINK DIRETTO ALL'UTENTE
-                        }
+                            </div>"; //LINK DIRETTO ALL'UTENTE
                     }
-                } else{
-                    echo "<h1>errore nella query</h1>";
-                    echo "<p>$sql</p>";
                 }
+            } else {
+                echo "<h1>errore nella query</h1>";
+                echo "<p>$sql</p>";
+            }
             ?>
         </div>
     </div>
